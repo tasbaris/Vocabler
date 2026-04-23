63b12e3e4a467b46fc2d31666f3b7c4e0adbc572
umutbalkaya2142 <umutbalkayax@gmail.com>
2026-04-24 00:48:58 +0300

    login/logout entegrasyonu

---
+++ frontend/assets/js/auth.js
@@ -0,0 +1,73 @@
+
+// if (window.location.pathname.includes('index.html') && !localStorage.getItem('user')) {
+//     window.location.href = 'login.html';
+// }
+
+// Backend API'nizin temel adresi (backand ci arkadaş burayı söyleyecek)
+const API_BASE_URL = 'http://localhost/vocabler_backend/api'; 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
+
+    if (loginForm) {
+        // Form "submit" (gönder) olayını dinliyoruz
+        loginForm.addEventListener('submit', async (e) => {
+            
+            e.preventDefault(); 
+
+            //  "Giriş Yapılıyor..." animasyonu için
+            const submitBtn = document.getElementById('loginBtn');
+            const originalBtnText = submitBtn.innerHTML;
+            submitBtn.disabled = true;
+            submitBtn.innerHTML = '<i class="fas fa-spinner fa-spin"></i> Giriş Yapılıyor...';
+
+            //backand için veirlei istenen form yapıyoz
+            const formData = new FormData(loginForm);
+            const loginData = Object.fromEntries(formData.entries());
+
+            try {  //burada backand e yolluyoz
+                const response = await fetch(`${API_BASE_URL}/login.php`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(loginData) // JSON paketimizi yolluyoruz
+                });
+
+                const result = await response.json();
+
+                if (response.ok && result.success) {
+                    // Giriş başarılı! Token/Kullanıcı bilgisini tarayıcıya kaydet
+                    localStorage.setItem('user', JSON.stringify(result.user)); //
+                    window.location.href = 'index.html'; // Anasayfaya yönlendir
+                } else {
+                    alert(`Giriş Başarısız: ${result.message}`);
+                }
+
+            } catch (error) {
+                console.error('Bağlantı hatası:', error);
+                alert('Sunucuya ulaşılamıyor. Lütfen bağlantınızı kontrol edin.');
+            } finally {
+                // İşlem bitince butonu eski haline (tıklanabilir) getirir
+                submitBtn.disabled = false;
+                submitBtn.innerHTML = originalBtnText;
+            }
+        });
+    }
+});
+
+const logoutBtn = document.getElementById('logoutBtn');
+
+if (logoutBtn) {
+    logoutBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        // Hafızayı temizleme
+        localStorage.removeItem('user');
+        
+        // Giriş sayfasına yönlendir
+        window.location.href = 'login.html';
+        
+        console.log("Çıkış yapıldı ve hafıza temizlendi.");
+    });
+}
